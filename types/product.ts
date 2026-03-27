e4308aa8d112c5db4f48b7294dac0767700b5a77
Daniel Salyik <salyik.daniel@hotmail.com>
2026-03-27 06:47:02 +0100

    Implement HomePage class with product sorting and selection functions, add types for Brands, Products, and Categories

---
+++ types/product.ts
@@ -0,0 +1,6 @@
+export type Product = {
+    name : string,
+    price : number,
+    co2Rating : string,
+    isEcofriendly : boolean
+}
---
+++ types/sortOptions.ts
@@ -0,0 +1,9 @@
+export enum SortOptions {
+  NameAscending = "name,asc",
+  NameDescending = "name,desc",
+  PriceAscending = "price,asc",
+  PriceDescending = "price,desc",
+  Co2Ascending = "co2_rating,asc",
+  Co2Descending = "co2_rating,desc"
+
+}

cf1c3cb3abeff0e863622e4db9cf8943d7ad6002
Daniel Salyik <salyik.daniel@hotmail.com>
2026-06-02 23:55:40 +0200

    feat: implement account page and add testcases for login functionality

--- tests/fixtures/index.ts
+++ tests/fixtures/index.ts
@@ -1,15 +1,20 @@
-import { test as base } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import { HomePage } from '../../pages/home.page';
 import { ContactPage } from '../../pages/contact.page';
 import { RegistrationPage } from '../../pages/registration.page';
 import { LoginPage } from '../../pages/login.page';
+import { AccountPage } from '../../pages/account.page';
+import env from '../../utils/env';
 
 type Fixtures = {
     homePage : HomePage,
     contactPage : ContactPage,
     registrationPage : RegistrationPage,
     loginPage : LoginPage
+    accountPage : AccountPage
 }
+
+export { expect };
 
 export const test = base.extend<Fixtures>({
     homePage : async({page}, use) => {
@@ -19,32 +24,39 @@ export const test = base.extend<Fixtures>({
         await use(homePage);
 
     },
-    contactPage : async({browser}, use) => {
+    contactPage : async({page}, use) => {
 
-        const context = await browser.newContext();
-        const page = await context.newPage();
         const contactPage = new ContactPage(page);
 
         await contactPage.goto();
         await use(contactPage);
-        await context.close(); 
+        
     },
-    registrationPage : async({browser}, use) => {
-        const context = await browser.newContext();
-        const page = await context.newPage();
+    registrationPage : async({page}, use) => {
+        
         const registrationPage = new RegistrationPage(page);
 
         await registrationPage.goto();
         await use(registrationPage);
-        await context.close();
     },
-    loginPage : async ({browser}, use)=> {
-        const context = await browser.newContext();
-        const page = await context.newPage();
+    loginPage : async({page}, use)=> {
+        
         const loginPage = new LoginPage(page);
 
-        await loginPage.goto();
+        await loginPage.gotoLoginPage();
         await use(loginPage);
-        await context.close();
+        
+    },
+    accountPage : async({page}, use) => {
+
+        const loginPage = new LoginPage(page);
+
+        await loginPage.gotoLoginPage();
+        await loginPage.login(env.REGISTERED_EMAIL_FOR_USER1, env.PASSWORD_FOR_USER1);
+
+        const accountPage = new AccountPage(page);
+        
+        await use(accountPage);
+
     }
 });
---
+++ tests/specs/login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '../fixtures'
+import env from '../../utils/env';
+
+
+test(`user can login with valid credentials`, async({loginPage, page}) => {
+    
+    await loginPage.login(env.REGISTERED_EMAIL_FOR_USER1, env.PASSWORD_FOR_USER1);
+
+    await expect(page).toHaveURL("/account");
+    
+    
+})
+
+test(`user cannot loggin with invalid credentials`, async({loginPage}) => {
+    
+    await loginPage.login("invalid@test.com", "invalid1030");
+
+
+    await expect(loginPage.errorMessage).toContainText("Invalid");
+
+})
